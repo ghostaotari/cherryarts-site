0a84e6309150cac1e4532b5c6367e358c449e416
cherrysantos <cherritoast@gmail.com>
2025-03-21 05:24:55 -0600

    some basics added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-  content: ["./src/**/*.{html,js,scss}", "./index.html"],
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx,vue,svelte,scss}",
+    "./index.html",
+  ],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Work Sans", "sans-serif"],
-      }
+        inter: ["Inter", "san-serif"],
+      },
+      colors: {
+        cherry: "#EC1C23",
+      },
     },
   },
   plugins: [],
-}
+};
